6b532ef072bfc948817072e44fb45eadab248edf
Emil <emilpradeep@gmail.com>
2025-06-20 18:33:59 +0530

    initial commit

---
+++ src/main/java/com/xperia/xpense_consumer/XpenseConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.xperia.xpense_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpenseConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpenseConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xperia/xpense_consumer/XpenseConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xperia.xpense_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpenseConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
