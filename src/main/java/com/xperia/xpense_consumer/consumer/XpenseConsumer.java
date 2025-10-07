341ced0fae3bf156a608fa3a3aad088bcd10536a
Emil <emilpradeep@gmail.com>
2025-10-07 11:46:41 +0530

    provided batch insert

--- src/main/java/com/xperia/xpense_consumer/consumer/XpenseConsumer.java
+++ src/main/java/com/xperia/xpense_consumer/consumer/XpenseConsumer.java
@@ -2,12 +2,15 @@ package com.xperia.xpense_consumer.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xperia.xpense_consumer.models.MutualFundSchemeConsumerModel;
+import com.xperia.xpense_consumer.models.entity.MutualFundScheme;
+import com.xperia.xpense_consumer.service.MutualFundSchemeService;
 import jakarta.annotation.PostConstruct;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.util.ArrayList;
@@ -20,12 +23,17 @@ import java.util.concurrent.Executors;
 @Component
 public class XpenseConsumer {
 
+    @Autowired
+    private MutualFundSchemeService mutualFundSchemeService;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(XpenseConsumer.class);
     private final Properties consumerProperties;
     private KafkaConsumer<String, String> consumer;
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
     private volatile boolean running = true;
     private final ObjectMapper objectMapper;
+    private final static long BATCH_SIZE = 1000;
+
 
     public XpenseConsumer(Properties kafkaConsumerProperties){
         this.consumerProperties = kafkaConsumerProperties;
@@ -36,7 +44,7 @@ public class XpenseConsumer {
     public void start(){
         consumer = new KafkaConsumer<>(consumerProperties);
         consumer.subscribe(Collections.singletonList("mf_scheme"));
-        List<MutualFundSchemeConsumerModel> list = new ArrayList<>();
+        List<MutualFundScheme> list = new ArrayList<>();
         executorService.submit(() -> {
             try{
                 while (running){
@@ -46,6 +54,13 @@ public class XpenseConsumer {
                             String json = record.value();
                             MutualFundSchemeConsumerModel model =
                                     objectMapper.readValue(json, MutualFundSchemeConsumerModel.class);
+                            MutualFundScheme mfScheme = new MutualFundScheme(model.getSchemeCode(), model.getSchemeName(),
+                                    model.getIsinGrowth(), model.getIsinDivReinvestment());
+                            list.add(mfScheme);
+                            if (list.size() >= BATCH_SIZE){
+                                mutualFundSchemeService.saveAllSchemes(list);
+                                list.clear();
+                            }
                         } catch (Exception e) {
                             LOGGER.error("Failed to parse record : {}", record.value(), e);
                         }
