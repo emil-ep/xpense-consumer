163fe2dfdb0cad562f3af438663902a5a2e9b151
Emil <emilpradeep@gmail.com>
2026-08-27 15:37:03 +0530

    removed unused import

--- src/main/java/com/xperia/xpense_consumer/consumer/MutualFundSchemeConsumer.java
+++ src/main/java/com/xperia/xpense_consumer/consumer/MutualFundSchemeConsumer.java
@@ -5,7 +5,6 @@ import jakarta.annotation.PostConstruct;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.postgresql.jdbc.TimestampUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
